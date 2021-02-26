60377ebc62ee6f7d1037034745a535a67ea4d525
Laksh-Koundinya <lakshmisn.08@gmail.com>
2021-02-26 08:38:26 +0530

    Added Projects Links

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
+	// if (scroll_position > 250) {
+	// 	header.style.backgroundColor = '#29323c';
+	// } else {
+	// 	header.style.backgroundColor = 'transparent';
+	// }
 });
 
 menu_item.forEach((item) => {
